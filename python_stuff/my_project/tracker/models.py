4c4b39ed89a9578328bc7f49c4db23df6d9013ed
iiggrimes <iiggrimes@gmail.com>
2024-06-19 18:18:16 -0400

    first commit pt2

---
+++ python_stuff/my_project/tracker/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    access_token = models.CharField(max_length=255)
+    item_id = models.CharField(max_length=100)
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    category = models.CharField(max_length=100)
+
---
+++ python_stuff/my_project/tracker/views.py
@@ -0,0 +1,20 @@
+#from django.shortcuts import render
+# Create your views here.
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import RegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'tracker/register.html', {'form': form})
